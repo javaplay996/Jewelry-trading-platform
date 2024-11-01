e0a035b442d38cae9e9937621813984a1d673262
杨荧 <java996@artskyhome.com>
2024-11-01 15:45:17 +0800

    源码上传

---
+++ db.sql
@@ -0,0 +1,301 @@
+/*
+SQLyog Ultimate v11.3 (64 bit)
+MySQL - 5.7.32-log : Database - zhubaoshoushijiaoyipingtai
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`zhubaoshoushijiaoyipingtai` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `zhubaoshoushijiaoyipingtai`;
+
+/*Table structure for table `address` */
+
+DROP TABLE IF EXISTS `address`;
+
+CREATE TABLE `address` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `address` varchar(200) NOT NULL COMMENT '地址',
+  `name` varchar(200) NOT NULL COMMENT '收货人',
+  `phone` varchar(200) NOT NULL COMMENT '电话',
+  `isdefault` varchar(200) NOT NULL COMMENT '是否默认地址[是/否]',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1616154684655 DEFAULT CHARSET=utf8 COMMENT='地址';
+
+/*Data for the table `address` */
+
+insert  into `address`(`id`,`addtime`,`userid`,`address`,`name`,`phone`,`isdefault`) values (1616154251293,'2021-03-19 19:44:11',1614430386281,'河南省郑州市二七区解放路街道金博大城C座金博大城D座','李憨憨','17703786932','是'),(1616154684654,'2021-03-19 19:51:24',1616138618446,'河南省郑州市金水区杜岭街道幼师家属院照相器材大世界','张涵涵','17703786969','是');
+
+/*Table structure for table `cart` */
+
+DROP TABLE IF EXISTS `cart`;
+
+CREATE TABLE `cart` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `tablename` varchar(200) DEFAULT 'shangpinxinxi' COMMENT '商品表名',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `goodid` bigint(20) NOT NULL COMMENT '商品id',
+  `goodname` varchar(200) DEFAULT NULL COMMENT '商品名称',
+  `picture` varchar(200) DEFAULT NULL COMMENT '图片',
+  `buynumber` int(11) NOT NULL COMMENT '购买数量',
+  `price` float DEFAULT NULL COMMENT '单价',
+  `discountprice` float DEFAULT NULL COMMENT '会员价',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1616154909635 DEFAULT CHARSET=utf8 COMMENT='购物车表';
+
+/*Data for the table `cart` */
+
+/*Table structure for table `config` */
+
+DROP TABLE IF EXISTS `config`;
+
+CREATE TABLE `config` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(100) NOT NULL COMMENT '配置参数名称',
+  `value` varchar(100) DEFAULT NULL COMMENT '配置参数值',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COMMENT='配置文件';
+
+/*Data for the table `config` */
+
+insert  into `config`(`id`,`name`,`value`) values (24,'picture2','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152801388.jpg'),(26,'picture2','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616154619554.jpg');
+
+/*Table structure for table `discussshangpinxinxi` */
+
+DROP TABLE IF EXISTS `discussshangpinxinxi`;
+
+CREATE TABLE `discussshangpinxinxi` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `refid` bigint(20) NOT NULL COMMENT '关联表id',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `content` longtext NOT NULL COMMENT '评论内容',
+  `reply` longtext COMMENT '回复内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1615612075811 DEFAULT CHARSET=utf8 COMMENT='商品评论表';
+
+/*Data for the table `discussshangpinxinxi` */
+
+/*Table structure for table `jifenjilu` */
+
+DROP TABLE IF EXISTS `jifenjilu`;
+
+CREATE TABLE `jifenjilu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `yonghu_id` bigint(20) NOT NULL COMMENT '用户id',
+  `shangpinxinxi_id` bigint(20) NOT NULL COMMENT '商品id',
+  `jifenjilu_types` varchar(200) DEFAULT NULL COMMENT '积分类型',
+  `buynumber` int(11) NOT NULL COMMENT '积分数量',
+  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录时间',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8 COMMENT='积分';
+
+/*Data for the table `jifenjilu` */
+
+insert  into `jifenjilu`(`id`,`yonghu_id`,`shangpinxinxi_id`,`jifenjilu_types`,`buynumber`,`insert_time`,`create_time`) values (54,1614430386281,1616152489258,'添加',10,'2021-03-19 19:45:05','2021-03-19 19:45:05'),(55,1616138618446,1616152489258,'添加',20,'2021-03-19 19:52:17','2021-03-19 19:52:17'),(56,1616138618446,1616152489258,'添加',20,'2021-03-19 19:55:26','2021-03-19 19:55:26'),(57,1616138618446,1616152489258,'使用',20,'2021-03-19 19:55:26','2021-03-19 19:55:26');
+
+/*Table structure for table `news` */
+
+DROP TABLE IF EXISTS `news`;
+
+CREATE TABLE `news` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `title` varchar(200) NOT NULL COMMENT '标题',
+  `picture` varchar(200) NOT NULL COMMENT '图片',
+  `content` longtext NOT NULL COMMENT '内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8 COMMENT='新闻资讯';
+
+/*Data for the table `news` */
+
+insert  into `news`(`id`,`addtime`,`title`,`picture`,`content`) values (81,'2021-03-15 22:39:58','新闻标题1','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/news_picture1.jpg','内容1'),(82,'2021-03-15 22:39:58','新闻标题2','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/news_picture2.jpg','内容2'),(83,'2021-03-15 22:39:58','新闻标题3','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/news_picture3.jpg','内容3'),(84,'2021-03-15 22:39:58','新闻标题4','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/news_picture4.jpg','内容4'),(85,'2021-03-15 22:39:58','新闻标题5','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/news_picture5.jpg','内容5');
+
+/*Table structure for table `orders` */
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `orderid` varchar(200) NOT NULL COMMENT '订单编号',
+  `tablename` varchar(200) DEFAULT 'shangpinxinxi' COMMENT '商品表名',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `goodid` bigint(20) NOT NULL COMMENT '商品id',
+  `goodname` varchar(200) DEFAULT NULL COMMENT '商品名称',
+  `picture` varchar(200) DEFAULT NULL COMMENT '商品图片',
+  `buynumber` int(11) NOT NULL COMMENT '购买数量',
+  `price` float NOT NULL DEFAULT '0' COMMENT '价格/积分',
+  `discountprice` float DEFAULT '0' COMMENT '折扣价格',
+  `total` float NOT NULL DEFAULT '0' COMMENT '总价格/总积分',
+  `discounttotal` float DEFAULT '0' COMMENT '折扣总价格',
+  `type` int(11) DEFAULT '1' COMMENT '支付类型',
+  `status` varchar(200) DEFAULT NULL COMMENT '状态',
+  `address` varchar(200) DEFAULT NULL COMMENT '地址',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `orderid` (`orderid`)
+) ENGINE=InnoDB AUTO_INCREMENT=1616154926691 DEFAULT CHARSET=utf8 COMMENT='订单';
+
+/*Data for the table `orders` */
+
+insert  into `orders`(`id`,`addtime`,`orderid`,`tablename`,`userid`,`goodid`,`goodname`,`picture`,`buynumber`,`price`,`discountprice`,`total`,`discounttotal`,`type`,`status`,`address`) values (1616154273311,'2021-03-19 19:44:32','202131919443266239764','shangpinxinxi',1614430386281,1616152489258,'宝泉珠宝','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152446983.webp',2,1000,999,2000,1998,1,'已发货','河南省郑州市二七区解放路街道金博大城C座金博大城D座'),(1616154305540,'2021-03-19 19:45:05','20213191945524360350','shangpinxinxi',1614430386281,1616152489258,'宝泉珠宝','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152446983.webp',1,1000,999,1000,999,1,'已完成','河南省郑州市二七区解放路街道金博大城C座金博大城D座'),(1616154700474,'2021-03-19 19:51:40','202131919514031471281','shangpinxinxi',1616138618446,1616152553563,'海洋之心项链','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152508263.webp',1,1000,998,1000,998,1,'未支付','河南省郑州市金水区杜岭街道幼师家属院照相器材大世界'),(1616154737888,'2021-03-19 19:52:17','202131919521751969411','shangpinxinxi',1616138618446,1616152489258,'宝泉珠宝','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152446983.webp',2,1000,999,2000,1998,1,'已完成','河南省郑州市金水区杜岭街道幼师家属院照相器材大世界'),(1616154926690,'2021-03-19 19:55:26','202131919552576094787','shangpinxinxi',1616138618446,1616152489258,'宝泉珠宝','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152446983.webp',2,1000,999,2000,1998,1,'已支付','河南省郑州市金水区杜岭街道幼师家属院照相器材大世界');
+
+/*Table structure for table `shangpinfenlei` */
+
+DROP TABLE IF EXISTS `shangpinfenlei`;
+
+CREATE TABLE `shangpinfenlei` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `shangpinfenlei` varchar(200) DEFAULT NULL COMMENT '商品分类',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1616154499776 DEFAULT CHARSET=utf8 COMMENT='商品分类';
+
+/*Data for the table `shangpinfenlei` */
+
+insert  into `shangpinfenlei`(`id`,`addtime`,`shangpinfenlei`) values (21,'2021-03-19 22:39:58','手饰'),(22,'2021-03-19 22:39:58','吊坠'),(23,'2021-03-19 22:39:58','耳饰'),(24,'2021-03-19 22:39:58','挂饰'),(1616154499775,'2021-03-19 19:48:19','手环');
+
+/*Table structure for table `shangpinxinxi` */
+
+DROP TABLE IF EXISTS `shangpinxinxi`;
+
+CREATE TABLE `shangpinxinxi` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `shangpinbianhao` varchar(200) DEFAULT NULL COMMENT '商品编号',
+  `shangpinmingcheng` varchar(200) DEFAULT NULL COMMENT '商品名称',
+  `shangpinfenlei` varchar(200) DEFAULT NULL COMMENT '商品分类',
+  `shuliang` varchar(200) DEFAULT NULL COMMENT '数量',
+  `pinpai` varchar(200) DEFAULT NULL COMMENT '品牌',
+  `guige` varchar(200) DEFAULT NULL COMMENT '规格',
+  `xiangqing` longtext COMMENT '详情',
+  `fengmian` varchar(200) DEFAULT NULL COMMENT '封面',
+  `clicktime` datetime DEFAULT NULL COMMENT '最近点击时间',
+  `clicknum` int(11) DEFAULT '0' COMMENT '点击次数',
+  `price` float NOT NULL COMMENT '原价',
+  `discountprice` float DEFAULT NULL COMMENT '折扣价',
+  `jifen` float DEFAULT NULL,
+  `flag` int(11) DEFAULT NULL COMMENT '是否上架(1:上架,2:下架)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `shangpinbianhao` (`shangpinbianhao`)
+) ENGINE=InnoDB AUTO_INCREMENT=1616154549309 DEFAULT CHARSET=utf8 COMMENT='商品信息';
+
+/*Data for the table `shangpinxinxi` */
+
+insert  into `shangpinxinxi`(`id`,`addtime`,`shangpinbianhao`,`shangpinmingcheng`,`shangpinfenlei`,`shuliang`,`pinpai`,`guige`,`xiangqing`,`fengmian`,`clicktime`,`clicknum`,`price`,`discountprice`,`jifen`,`flag`) values (1616152489258,'2021-03-19 19:14:48','1616152399','宝泉珠宝','手饰','993','宝泉','大','保全珠宝的详情\r\n\r\n','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152446983.webp','2021-03-19 20:23:14',12,1000,999,10,1),(1616152553563,'2021-03-19 19:15:52','1616152492','海洋之心项链','挂饰','19','海洋之心','大','海洋之心项链详情\r\n','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152508263.webp','2021-03-19 20:24:10',3,1000,998,12,1),(1616152608930,'2021-03-19 19:16:48','1616152565','金坠','吊坠','50','伊犁','大','金坠的详情\r\n','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152570396.webp',NULL,0,1000,888,10,1),(1616152660529,'2021-03-19 19:17:39','1616152612','丘比特之箭项链','挂饰','10','伊犁','大','丘比特之箭详情\r\n','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152619233.webp','2021-03-19 19:55:54',4,999,888,11,1),(1616152713806,'2021-03-19 19:18:33','1616152664','似水柔情项链','挂饰','20','伊犁','大','似水柔性详情\r\n','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152671212.webp',NULL,0,999,777,10,1),(1616154549308,'2021-03-19 19:49:08','1616154511','玉镯子','手环','14','1','大','玉镯子详情\r\n','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616154522173.webp','2021-03-19 19:49:50',2,1000,666,10,1);
+
+/*Table structure for table `shouhou` */
+
+DROP TABLE IF EXISTS `shouhou`;
+
+CREATE TABLE `shouhou` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `orderid` varchar(200) NOT NULL COMMENT '订单id',
+  `suqiu` varchar(200) NOT NULL COMMENT '诉求内容',
+  `suqiutime` timestamp NULL DEFAULT NULL COMMENT '诉求时间',
+  `shouhoutype` int(11) NOT NULL COMMENT '售后类型 1:已处理  2 未处理',
+  `chulitype` int(11) DEFAULT NULL COMMENT '处理结果  1:已解决  2:未解决',
+  `chulicontent` varchar(200) DEFAULT NULL,
+  `chulitime` timestamp NULL DEFAULT NULL COMMENT '处理时间',
+  `addtime` timestamp NULL DEFAULT NULL COMMENT '添加时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1372878778255872003 DEFAULT CHARSET=utf8 COMMENT='售后表';
+
+/*Data for the table `shouhou` */
+
+insert  into `shouhou`(`id`,`orderid`,`suqiu`,`suqiutime`,`shouhoutype`,`chulitype`,`chulicontent`,`chulitime`,`addtime`) values (1372877054300459010,'20213191945524360350','质量不好','2021-03-19 19:46:11',1,2,'思思思思','2021-03-19 19:46:22','2021-03-19 19:46:11'),(1372878778255872002,'202131919521751969411','质量不好','2021-03-19 19:53:02',1,1,'处理好了','2021-03-19 19:53:21','2021-03-19 19:53:02');
+
+/*Table structure for table `storeup` */
+
+DROP TABLE IF EXISTS `storeup`;
+
+CREATE TABLE `storeup` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `refid` bigint(20) DEFAULT NULL COMMENT '收藏id',
+  `tablename` varchar(200) DEFAULT NULL COMMENT '表名',
+  `name` varchar(200) NOT NULL COMMENT '收藏名称',
+  `picture` varchar(200) NOT NULL COMMENT '收藏图片',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1616154944296 DEFAULT CHARSET=utf8 COMMENT='收藏表';
+
+/*Data for the table `storeup` */
+
+insert  into `storeup`(`id`,`addtime`,`userid`,`refid`,`tablename`,`name`,`picture`) values (1616154944295,'2021-03-19 19:55:43',1616138618446,1616152660529,'shangpinxinxi','丘比特之箭项链','http://localhost:8080/zhubaoshoushijiaoyipingtai/upload/1616152619233.webp');
+
+/*Table structure for table `token` */
+
+DROP TABLE IF EXISTS `token`;
+
+CREATE TABLE `token` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `username` varchar(100) NOT NULL COMMENT '用户名',
+  `tablename` varchar(100) DEFAULT NULL COMMENT '表名',
+  `role` varchar(100) DEFAULT NULL COMMENT '角色',
+  `token` varchar(200) NOT NULL COMMENT '密码',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增时间',
+  `expiratedtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '过期时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COMMENT='token表';
+
+/*Data for the table `token` */
+
+insert  into `token`(`id`,`userid`,`username`,`tablename`,`role`,`token`,`addtime`,`expiratedtime`) values (9,1,'admin','users','管理员','aipugbqckk90phpda5mf1mnb48swka2h','2021-03-13 09:42:19','2021-03-19 21:21:39'),(10,1614430386281,'a2','yonghu','用户','0x72zmitu7i88ed9lqeb9bkhrdg30cz1','2021-03-13 09:47:59','2021-03-19 20:19:25'),(11,1615339905628,'a3','yonghu','用户','z2cdoexnum3kjy2nz6kmmbcgdlflajub','2021-03-13 11:02:07','2021-03-13 12:02:07'),(18,1616138618446,'a3','yonghu','用户','pyb1qjidfozqn8b6eg939c9yfmfr1i4f','2021-03-19 19:50:46','2021-03-19 21:23:27');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(100) NOT NULL COMMENT '用户名',
+  `password` varchar(100) NOT NULL COMMENT '密码',
+  `role` varchar(100) DEFAULT '管理员' COMMENT '角色',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`role`,`addtime`) values (1,'admin','123456','管理员','2021-03-09 22:39:58');
+
+/*Table structure for table `yonghu` */
+
+DROP TABLE IF EXISTS `yonghu`;
+
+CREATE TABLE `yonghu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `yonghuzhanghao` varchar(200) NOT NULL COMMENT '用户账号',
+  `yonghuxingming` varchar(200) NOT NULL COMMENT '用户姓名',
+  `mima` varchar(200) NOT NULL COMMENT '密码',
+  `xingbie` varchar(200) DEFAULT NULL COMMENT '性别',
+  `lianxidianhua` varchar(200) DEFAULT NULL COMMENT '联系电话',
+  `dianziyouxiang` varchar(200) DEFAULT NULL COMMENT '电子邮箱',
+  `money` float DEFAULT '0' COMMENT '余额',
+  `jifen` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `yonghuzhanghao` (`yonghuzhanghao`)
+) ENGINE=InnoDB AUTO_INCREMENT=1616154464633 DEFAULT CHARSET=utf8 COMMENT='用户';
+
+/*Data for the table `yonghu` */
+
+insert  into `yonghu`(`id`,`addtime`,`yonghuzhanghao`,`yonghuxingming`,`mima`,`xingbie`,`lianxidianhua`,`dianziyouxiang`,`money`,`jifen`) values (1614430386281,'2021-02-27 20:53:06','a2','a22','123456','男','13823888883','229992@qq.ccom',99001,10),(1616138618446,'2021-03-19 15:23:38','a3','张三','123456','女','17703786922','23333@qq.ccom',96024,20),(1616153976948,'2021-03-19 19:39:36','a4','张4','123456','男','17703786931','2@qq.ccom',0,0),(1616154464632,'2021-03-19 19:47:44','a6','张6','123456','男','17703786936','22@qq.ccom',0,0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
